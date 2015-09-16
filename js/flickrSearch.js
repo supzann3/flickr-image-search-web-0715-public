7620c9a660ad0631227982f54b826b597b3b2794
supzann3 <susan.hehe01@gmail.com>
2015-09-16 16:14:45 -0400

    Done.

--- js/flickrSearch.js
+++ js/flickrSearch.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
-  
   //write your solution here...
-    
-});
+  $('#search').click(function(){
+    jsonCall();
+  });
+  $('#keyword').keypress(function(e){
+    if(e.which==13){
+      e.preventDefault();
+      e.stopPropagation();
+      jsonCall();
+    }
+  });
 
+function jsonCall () {
+  var searchTerm=$('#keyword').val();
+  var url="https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=2fd41b49fedfd589dc265350521ab539&tags="+searchTerm+"&jsoncallback=?";
+  $.getJSON(url,function(data){
+    data.photos.photo.forEach(function(photo){
+      $('#feed').append('<img src="http://farm'+photo.farm +'.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'.jpg">');
+    });
+  });
+}
+});
 /*
 
-API url: 
+API url:
 
 https://www.flickr.com/services/api/request.rest.html
 
